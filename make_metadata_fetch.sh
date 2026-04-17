b535aa70f356a228534b734945a4a94c7a33bff5
Jonathon Pongsin <112664325+jpongsin@users.noreply.github.com>
2026-04-16 23:05:29 -0500

    Update make_metadata_fetch.sh for build folder handling
    Add build folder creation and cleanup in script

--- make_metadata_fetch.sh
+++ make_metadata_fetch.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+build_folder="build"
+
+if [ -d "$build_folder" ]; then
+	rm -rf "$build_folder"
+fi
+
+#ensure you are in main folder before building
+mkdir "$build_folder"
+cd "$build_folder"
+
 cmake --build . --parallel
